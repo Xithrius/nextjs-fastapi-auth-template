1480cb5f91ac8c72d8c35ff44dd3789ac7a5f2e0
Xithrius <xithrius@gmail.com>
2023-10-14 21:55:02 -0700

    Got the API running, addition of first migration

--- api/app/routers/application.py
+++ api/app/routers/application.py
@@ -1,5 +1,3 @@
-from importlib import metadata
-
 from fastapi import FastAPI
 from fastapi.responses import UJSONResponse
 
@@ -17,7 +15,7 @@ def get_app() -> FastAPI:
     """
     app = FastAPI(
         title="api",
-        version=metadata.version("api"),
+        version="0.1.0",
         docs_url="/api/docs",
         redoc_url="/api/redoc",
         openapi_url="/api/openapi.json",
